a8ad81dc8c0e7b9b643b23021ed3bee734711ce0
onion237 <ssafy1121@gmail.com>
2021-12-23 13:46:26 +0900

    sync

--- src/main/java/com/douzone/editorserver/controller/SyncDocumentController.java
+++ src/main/java/com/douzone/editorserver/controller/SyncDocumentController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -25,50 +27,143 @@ public class SyncDocumentController {
 	private DocumentService documentService;
 	
 	
-	private Map<Long, List<Map>> documentChanges = new HashMap<Long, List<Map>>();
-	
+	private ConcurrentMap<Long, List<Map>> documentChanges = new ConcurrentHashMap<Long, List<Map>>();
 	
 	@PostMapping("/pub/{docNo}")
-	synchronized public void pub(@PathVariable Long docNo, @RequestBody Map change, @AuthUser User authUser) throws InterruptedException {
-		System.out.println("-==========================start========================================================");
-		System.out.println(authUser);
+	public void pub(@PathVariable Long docNo, @RequestBody Map incomingChange, @AuthUser User authUser) throws InterruptedException {
+		Thread.sleep(1500);
 		
-		change.keySet().forEach((key) -> System.out.println("key : " + key + " , value : " + change.get(key)));		
-		change.put("user", authUser.getNo());
+		System.out.println(authUser);	
+		incomingChange.keySet().forEach((key) -> System.out.println("key : " + key + " , value : " + incomingChange.get(key)));		
+		incomingChange.put("user", authUser.getNo());
 
 		List<Map> changeList = documentChanges.get(docNo);
-		Thread.sleep(1000);
 		
-		if(changeList == null) {
-			System.out.println("changeList is Null");
-			changeList = new ArrayList<Map>();
+		System.out.println("-==========================start========================================================");
+		synchronized (changeList) {
+			Long version = null;
 			
-			Long version = documentService.getVersion(docNo);
-			change.put("version", version + 1);
+			if(changeList.size() == 0) {
+				System.out.println("changeList is Empty");
+				version = documentService.getVersion(docNo) + 1;
+			}else{
+				Map prevChange = changeList.get(changeList.size() - 1);
+				version = (Long)prevChange.get("version") + 1;			
+			}
+			incomingChange.put("version", version);
+			//OT - ops를 변환
+			//{retain} + {insert : (text | obj)}
+			//{retatn} + {delete : number}
+			//{retain} + {retain, attribute}
 			
-			documentChanges.put(docNo, changeList);
-		}else if(changeList.size() == 0){
-			System.out.println("changeList is Empty");
-			throw new RuntimeException("impossible when syncronized ");
-		}else {
-			Map prevChange = changeList.get(changeList.size() - 1);
-			change.put("version", (Long)prevChange.get("version") + 1);
-			System.out.println("the document is on the version " + change.get("version"));
+			Integer baseVersion = (Integer)incomingChange.get("baseVersion");
 			
-			//OT
+			Map oldChange = null;
+			List<Map> oldOPs = null;
+			
+			List<Map> incomingOPs = (List<Map>) ((Map)incomingChange.get("delta")).get("ops");
+			System.out.println(incomingOPs);
 			
 			
+			for(int i = changeList.size() - 1; i >= 0; i--) {
+				oldChange = changeList.get(i);
+				if((Long)oldChange.get("version") <= baseVersion){
+					System.out.println("=000000000000000000000000탈출11111111111111111111111111111111111");
+					break;
+				}
+				if(oldChange.get("sid").equals(incomingChange.get("sid"))) continue;
+				
+				// start
+				oldOPs = (List<Map>) ((Map)oldChange.get("delta")).get("ops");
+				
+				int incomingCursor = 0;
+				int len;
+				
+				if(!(incomingOPs.get(0).size() == 1 && incomingOPs.get(0).containsKey("retain"))) {
+					// index가 0인 연산
+					int offset = getTransformedOffset(0, oldOPs, (String)incomingChange.get("sid"), (String)oldChange.get("sid"));
+					if(offset != 0) {
+						Map retainOP = new HashMap<>();
+						retainOP.put("retain", offset);
+						incomingOPs.add(0, retainOP);
+						incomingCursor += offset;
+					}
+				}
+				
+				for(Map incomingOP : incomingOPs) {			
+					if(incomingOP.containsKey("retain")) {
+						incomingCursor += (Integer)incomingOP.get("retain");
+					}
+					
+					if(incomingOP.containsKey("insert")) {
+						if(incomingOP.get("insert") instanceof String) {
+							len = ((String)incomingOP.get("insert")).length();
+						}else {
+							len = 1;
+						}
+						incomingCursor += len;
+					}
+					
+					// retain이 아니면 transform 대상이 아님
+					if(!(incomingOP.size() == 1 && incomingOP.containsKey("retain"))) continue;
+					
+					// transform
+					int offset = getTransformedOffset(incomingCursor, oldOPs, (String) incomingChange.get("sid"), (String) oldChange.get("sid"));
+					if(offset != 0) {
+						incomingOP.put("retain", (Integer)incomingOP.get("retain") + offset);						
+						incomingCursor += offset;
+					}
+					System.out.println("transformed");
+					System.out.println(incomingOP);
+				}
+				
+			}
+			changeList.add(incomingChange);
+			
+			simpMessagingTemplate.convertAndSend("/sub/" + docNo, incomingChange);
+			System.out.println("-==========================end========================================================");
 		}
-		
-		changeList.add(change);
-		simpMessagingTemplate.convertAndSend("/sub/" + docNo, change);
-		System.out.println("-==========================end========================================================");
 	}
 	
+	private int getTransformedOffset(Integer incomingCursor, List<Map> oldOPs, String incomingSid, String oldSid) {
+		Integer oldCursor = 0;
+		int offset = 0;
+		int len;
+		
+		for(Map oldOP : oldOPs) {
+			if(oldCursor > incomingCursor) break;
+			
+			//insert tie break - sid가 작은쪽이 앞으로 감
+			if(oldCursor == incomingCursor && incomingSid.compareTo(oldSid) < 0) break;
+			
+			if(oldOP.containsKey("retain")) {
+				oldCursor += (Integer)oldOP.get("retain");
+			}
+			
+			if(oldOP.containsKey("insert")) {
+				if(oldOP.get("insert") instanceof String) {
+					len = ((String)oldOP.get("insert")).length();
+				}else {
+					len = 1;
+				}
+				offset += len;
+				oldCursor += len;
+			}
+			
+			if(oldOP.containsKey("delete")) {
+				offset -= (Integer) oldOP.get("delete");
+			}
+		}
+		
+		return offset;
+	}
+
 	@GetMapping("/history/{docNo}")
 	public List getHistory(@PathVariable Long docNo, @AuthUser User authUser) {
 		System.out.println("888888888888");
 		System.out.println(documentChanges.get(docNo));
+		documentChanges.putIfAbsent(docNo, new ArrayList<Map>());
+		
 		return documentChanges.get(docNo);
 	}
 }
